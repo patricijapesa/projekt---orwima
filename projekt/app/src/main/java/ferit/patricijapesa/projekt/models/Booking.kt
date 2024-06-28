7c667abbfb2e67dc2364bb5dae777ceef9e243b9
patricijapesa <163465574+patricijapesa@users.noreply.github.com>
2024-06-28 07:07:51 +0200

    Delete projekt directory

--- projekt/app/src/main/java/ferit/patricijapesa/projekt/models/Booking.kt
+++
@@ -1,8 +0,0 @@
-package ferit.patricijapesa.projekt.models
-
-class Booking (
-    val bookingId: String = "",
-    val driveId: String = "",
-    val userId: String = "",
-    val status: String = ""
-)
--- projekt/app/src/main/java/ferit/patricijapesa/projekt/models/User.kt
+++
@@ -1,9 +0,0 @@
-package ferit.patricijapesa.projekt.models
-
-class User (
-    val userId: String = "",
-    val name: String = "",
-    val email: String = "",
-    val profilePic: String = "",
-    val rating: Number
-)
